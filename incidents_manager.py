5e329b8a11b73dffdfb39f1d5d4882f5a86f5c29
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2017-11-27 17:15:30 +0100

    Bootstrap ElasticSearch configuration and index creation

--- incidents_manager.py
+++ incidents_manager.py
@@ -1,18 +1,47 @@
 from log import log
 
+from config import config
+
+# ElasticSearch
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+from aws_requests_auth import boto_utils
+
 
 class IncidentsManager(object):
     def __init__(self):
-        log.info("Initializing incidents manager...")
+        log.info("Initializing incidents manager ...")
         # Slack
         # SMTP configuration
         # Elasticsearch
+        log.info("- Configuring Elasticsearch connection ...")
+        self.es_index = config['elasticsearch']['index']
+        try:
+            aws_auth = AWSRequestsAuth(
+                aws_host    = config['elasticsearch']['host'],
+                aws_region  = config['elasticsearch']['region'],
+                aws_service = 'es',
+                **boto_utils.get_credentials()
+            )
+            self.es = Elasticsearch(
+                hosts   = [{'host': config['elasticsearch']['host'], 'port': 443}],
+                http_auth           = aws_auth,
+                use_ssl             = True,
+                verify_certs        = True,
+                connection_class    = RequestsHttpConnection
+            )
+        except:
+            log.error("Couldn't connect to Elasticsearch")
+        try:
+            self.create_es_index(self.es_index)
+        except:
+            log.error("Couldn't create Elasticsearch index")
         # Cachet
         return
 
     def create_incident(self, priority, title, description):
         log.info(
-            "Creating incident...:\n"
+            "Creating incident ...:\n"
             "  Priority: {priority}\n"
             "  Title: {title}\n"
             "  Description: {description}"
@@ -21,13 +50,13 @@ class IncidentsManager(object):
         log.info("Created incident successfully :)")
 
     def close_incident(self, event):
-        log.info("Closing incident...")
+        log.info("Closing incident ...")
         source = self.extract_event_infos(event)
         log.info("Source: " + str(source))
         log.info("Closed incident successfully :)")
 
     def list_incident_updates(self, event):
-        log.info("Listing incident updates...")
+        log.info("Listing incident updates ...")
         source = self.extract_event_infos(event)
         log.info("Source: " + str(source))
 
@@ -50,3 +79,14 @@ class IncidentsManager(object):
             'user': source_user,
             'message': source_message
         }
+
+    def create_es_index(self, es_index):
+        # === Wipe index
+        #self.es.indices.delete(index=self.es_index)
+        # === Create it again
+        print("Creating index " + es_index + " ...")
+        index_creation = self.es.indices.create(index=es_index, ignore=400)
+        if 'acknowledged' in index_creation and index_creation['acknowledged']:
+            print("... Index created")
+        elif 'status' in index_creation and index_creation['status'] == 400:
+            print("... Index already exists, continuing")
